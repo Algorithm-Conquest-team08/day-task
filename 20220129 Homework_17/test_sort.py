ae1f432bec1578be1867161c1aa0a34af51bd66f
kkamangdol <97426034+kkamangdol@users.noreply.github.com>
2022-01-29 17:27:50 +0900

    Delete 20220129 Homework_17 directory

--- 20220129 Homework_17/test_sort.py
+++
@@ -1,8 +0,0 @@
-from sorts import bubblesort,selectionsort
-
-# assert bubblesort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
-# assert bubblesort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
-
-assert selectionsort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
-assert selectionsort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
-
